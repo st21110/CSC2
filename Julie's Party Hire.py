9ab05fd10d1b45917267574c246f851909af4c97
st21110 <99319649+st21110@users.noreply.github.com>
2022-06-03 09:42:11 +1200

    Add files via upload

---
+++ Julie's Party Hire.py
@@ -0,0 +1,17 @@
+#Date: 6/1/22
+#Author: Prisha
+
+"""
+Julie's Party Hire. 
+
+This code will use functions along side methods to...
+
+"""
+
+#imports for the project
+from tkinter import Tk
+root = Tk()
+
+#Naming the root
+root.title = "Julie's Party Hire"
+
